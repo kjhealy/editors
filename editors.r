8998fca32e25e712c65d4e9bc0f5b7a8e03535d3
Kieran Healy <kjhealy@gmail.com>
2012-04-17 20:43:02 -0400

    pretty decent clustering

--- editors.r
+++ editors.r
@@ -12,22 +12,26 @@ library(ggplot2)
 library(scales)
 library(reshape)
 
-out.by.name <- agnes(data, method="ward")
-out.by.feature <- agnes(as.data.frame(t(data)), metric="manhattan", method="ward")
-
+d <- daisy(data)
+d1 <- daisy(as.data.frame(t(data)))
+out.by.name <- agnes(d, method="ward")
+out.by.name <- hclust(d, method="ward")
+out.by.feature <- agnes(d1, method="ward")
+out.by.feature <- hclust(d1, method="ward")
 
 ## out.by.name <- diana(data, metric="manhattan")
 ## out.by.feature <- diana(as.data.frame(t(data)), metric="manhattan")
+## plot(h)
 
 
-plot(out.by.name)
-plot(out.by.feature)
+##plot(out.by.name)
+##plot(out.by.feature)
 
 o.row <- out.by.name$order
 o.col <- out.by.feature$order
 
-data.o <- data[o.row,(ncol(data.o):1)] ## sort by clustered Editor only, features in BT's original order
-## data.o <- data[o.row, o.col] ## sort by clustered Editor and clustered Feature
+## data.o <- data[o.row,(ncol(data):1)] ## cluster on Editors only, features in BT's original order
+data.o <- data[o.row, o.col] ## sort based on clustering of Editors and Features
 
 ## Clean the labels for use below
 feature.labels <- gsub("\\."," ", colnames(data.o))
@@ -48,13 +52,19 @@ colnames(data.melt) <- c("Editor", "Feature", "Present")
 
 library(gdata)
 data.melt$Present <- reorder.factor(data.melt$Present,
-                                     new.order=c("No", "Yes", "$$", "?"))
+                                     new.order=c("Yes", "No", "$$", "?"))
 
 ## No need for this if not clustering on features
-#data.melt$Feature <- reorder.factor(data.melt$Feature, new.order=c.names)
+data.melt$Feature <- reorder.factor(data.melt$Feature, new.order=c.names)
 detach(package:gdata)
 
-pdf(file="figures/spec-cluster.pdf",height=9,width=16,pointsize=10)
+my.cols <- brewer.pal(9, "Pastel1")
+my.cols <- my.cols[c(3,1,5,9)]
+
+pdf(file="figures/spec-cluster.pdf",height=9,width=16,pointsize=11)
 p <- ggplot(data.melt, aes(x=Feature, y=Editor, fill=Present))
-p + geom_tile() + scale_x_discrete(labels=feature.labels) + opts(axis.text.x=theme_text(hjust=1, angle=90), axis.text.y=theme_text(hjust=1)) + coord_flip()
+p + geom_tile() + scale_fill_manual(values=my.cols) +
+  scale_x_discrete(labels=feature.labels) +
+  opts(axis.text.x=theme_text(hjust=1, angle=90),
+       axis.text.y=theme_text(hjust=1)) + coord_flip()
 dev.off()
